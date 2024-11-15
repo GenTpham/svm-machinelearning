31b43dfff9a541aeb3f9298600b2e17302b7de7f
GenTpham <58386839+GenTpham@users.noreply.github.com>
2024-11-15 16:35:12 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,3 +19,34 @@ with st.expander('Data'):
   st.write('**y**')
   y_raw = df.Class
   y_raw
+
+with st.sidebar:
+  st.header('Input fearures')
+  #Clump,UnifSize,UnifShape,MargAdh,SingEpiSize,BareNuc,BlandChrom,NormNuc,Mit
+  Clump = st.slider('Clump', 0, 20, 10)
+  UnifSize = st.slider('UnifSize', 0, 20, 10)
+  UnifShape = st.slider('UnifShape', 0, 20, 10)
+  MargAdh = st.slider('MargAdh', 0, 20, 10)
+  SingEpiSize = st.slider('SingEpiSize', 0,20,10)
+  BareNuc = st.slider('BareNuc', 0,20,10)
+  BlandChrom = st.slider('BlandChrom', 0, 20, 10)
+  NormNuc = st.slider('NormNuc', 0,20,10)
+  Mit = st.slider('Mit', 0,20,10)
+
+  data = {'Clump': Clump,
+          'UnifSize': UnifSize,
+          'UnifShape': UnifShape,
+          'MargAdh': MargAdh,
+          'SingEpiSize': SingEpiSize,
+          'BareNuc': BareNuc,
+          'BlandChrom': BlandChrom,
+          'NormNuc': NormNuc,
+          'Mit': Mit}
+  input_df = pd.DataFrame(data, index =[0])
+  input_cancer = pd.concat([input_df, X_raw], axis = 0)
+
+with st.expander('Input features'):
+  st.write('**Input cancer**')
+  input_df
+  st.write('**Combined cancer data**')
+  input_cancer
